530857daade9fbaf198c809927b19ef1fac434d8
Robert Knutsson <robert.knutsson@pricerunner.com>
2021-08-19 21:02:33 +0200

    remove i3-dmenu-desktop

--- travis/docs.sh
+++ travis/docs.sh
@@ -10,7 +10,6 @@ do
 	asciidoc -a linkcss -a stylesdir=https://i3wm.org/css -a scriptsdir=https://i3wm.org/js --backend=xhtml11 -f docs/asciidoc-git.conf $(dirname $f)/$(basename $f .html)
 done
 
-./docs/i3-pod2html --stylesurl=https://i3wm.org/css i3-dmenu-desktop man/i3-dmenu-desktop.html
 ./docs/i3-pod2html --stylesurl=https://i3wm.org/css i3-save-tree man/i3-save-tree.html
 ./docs/i3-pod2html --stylesurl=https://i3wm.org/css build/i3test.pm docs/lib-i3test.html
 ./docs/i3-pod2html --stylesurl=https://i3wm.org/css testcases/lib/i3test/Test.pm docs/lib-i3test-test.html
