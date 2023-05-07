df7fd6197b531d64860f54943eaa607189b3058a
Robert Knutsson <zybreak@gmail.com>
2023-05-07 08:20:52 +0200

    conan2

--- Makefile.sh
+++ Makefile.sh
@@ -3,6 +3,6 @@
 CONAN_PROFILE=default
 #export CXX=$HOME/.local/bin/clang++
 mkdir build && cd build
-conan install .. -pr $CONAN_PROFILE -s build_type=Debug --build=missing
+conan install .. -pr:b=$CONAN_PROFILE -s build_type=Debug --output-folder=. --build=missing
 cmake -G "Ninja Multi-Config" .. -DCMAKE_TOOLCHAIN_FILE=conan_toolchain.cmake
 cmake --build . --config Debug
