db7342e4d9429e21a84084ad83e115a514de9d4f
Robert Knutsson <zybreak@gmail.com>
2023-05-17 22:04:40 +0200

    use cmake presets

--- Makefile.sh
+++ Makefile.sh
@@ -1,9 +1,7 @@
 #!/bin/sh
-#CONAN_PROFILE=clang
 CONAN_PROFILE=default
-#export CXX=$HOME/.local/bin/clang++
 mkdir build
-conan install . -pr:b=$CONAN_PROFILE -s build_type=Debug --output-folder=build --build=missing
-cmake -G "Ninja Multi-Config" -DCMAKE_TOOLCHAIN_FILE=/home/zybreak/src/i3/build/conan_toolchain.cmake -B build
-cmake --build build --config Debug
+conan install . -pr:b=$CONAN_PROFILE -c:h tools.cmake.cmaketoolchain:generator="Ninja Multi-Config" -s build_type=Debug --output-folder=build --build=missing
+cmake --preset conan-default
+cmake --build build --config Debug --preset conan-debug
 ctest -C Debug --test-dir build
