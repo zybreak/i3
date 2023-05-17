7b94a197b43bcd1311153824c1ee41174673b9ee
Robert Knutsson <zybreak@gmail.com>
2023-05-17 20:59:39 +0200

    added tests to Makefile.sh

--- Makefile.sh
+++ Makefile.sh
@@ -6,3 +6,4 @@ mkdir build
 conan install . -pr:b=$CONAN_PROFILE -s build_type=Debug --output-folder=build --build=missing
 cmake -G "Ninja Multi-Config" -DCMAKE_TOOLCHAIN_FILE=/home/zybreak/src/i3/build/conan_toolchain.cmake -B build
 cmake --build build --config Debug
+ctest -C Debug --test-dir build
