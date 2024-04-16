ffbbbf347703a50c452d766a8bf7e4b12e7c1d72
yuvallangerontheroad <yuvallangerontheroad@gmail.com>
2024-04-16 14:38:23 +0300

    Add a newline at the end of the version option output. (#5980)
    If there is no newline character at the end of the version option's
    output, the next command line prompt is written left to the version,
    rather than under it.

--- i3-input/main.c
+++ i3-input/main.c
@@ -407,7 +407,7 @@ int main(int argc, char *argv[]) {
                 socket_path = sstrdup(optarg);
                 break;
             case 'v':
-                printf("i3-input " I3_VERSION);
+                printf("i3-input " I3_VERSION "\n");
                 return EXIT_OK;
             case 'p':
                 /* This option is deprecated, but will still work in i3 v4.1, 4.2 and 4.3 */
