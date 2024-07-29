90876aa5d03fe76587c7ac32c8a7ac8264c63468
Robert Knutsson <zybreak@gmail.com>
2024-07-29 14:50:29 +0200

    conan for more deps

--- conanfile.py
+++ conanfile.py
@@ -19,16 +19,21 @@ class i3(ConanFile):
         tc.generate()
 
 
+    def build_requirements(self):
+        self.test_requires("gtest/1.13.0")
+        
+
     def requirements(self):
         self.requires("zlib/1.2.13")
         self.requires("libev/4.33")
         self.requires("pcre2/10.42")
         self.requires("fmt/10.2.1", force=True)
         self.requires("antlr4-cppruntime/4.13.1")
-        self.requires("glib/2.76.2")
-        self.requires("gtest/1.13.0")
+        self.requires("glib/2.78.3")
         self.requires("spdlog/1.13.0")
         self.requires("nlohmann_json/3.11.3")
+        self.requires("cairo/1.18.0")
+        self.requires("pango/1.51.0")
 
 
     def layout(self):
