9c8a55114d87686f54dca3d246ba829afc3d4dab
Robert Knutsson <zybreak@gmail.com>
2024-08-15 16:39:07 +0200

    test json generator

--- i3/test/dump_node_test.cpp
+++ i3/test/dump_node_test.cpp
@@ -29,6 +29,58 @@ TEST(DumpNodeTest, Dump) {
     output.con_attach(&root, true, nullptr);
     
     json j = dump_node(&root, false);
+   
+    json nodes = json::array();
+    nodes.push_back({
+        {"actual_deco_rect",{{"height",0},{"width",0},{"x",0},{"y",0}}},
+        {"border","normal"},
+        {"current_border_width",-1},
+        {"deco_rect",{{"height",0},{"width",0},{"x",0},{"y",0}}},
+        {"floating","auto_off"},
+        {"floating_nodes",json::array()},
+        {"focus",json::array()},
+        {"focused",false},
+        {"fullscreen_mode",0},
+        {"geometry",{{"height",0},{"width",0},{"x",0},{"y",0}}},
+        {"id",(uintptr_t)&output},
+        {"last_split_layout","splith"},
+        {"layout","output"},
+        {"name","eDP-1"},
+        {"nodes",json::array()},
+        {"orientation","none"},
+        {"rect",{{"height",0},{"width",0},{"x",0},{"y",0}}},
+        {"sticky",false},
+        {"swallows",json::array()},
+        {"type","output"},
+        {"urgent",false},
+        {"window_icon_padding",-1},
+        {"window_rect",{{ "height",0 },{ "width",0 },{ "x",0 },{ "y",0 }}}
+    });
     
-    ASSERT_TRUE(j.is_object());
+    json j2 = {
+        {"border","normal"},
+        {"current_border_width",-1},
+        {"deco_rect", {{ "height",0 }, { "width",0 }, { "x",0 }, { "y",0 }}},
+        {"floating","auto_off"},
+        {"floating_nodes", json::array()},
+        {"focus",std::vector{(uintptr_t)&output}},
+        {"focused",false},
+        {"fullscreen_mode",0},
+        {"geometry",{{"height",0}, {"width",0}, {"x",0}, {"y",0}}},
+        {"id",(uintptr_t)&root},
+        {"last_split_layout","splitv"},
+        {"layout","splitv"},
+        {"name","root"},
+        {"nodes", nodes},
+        {"orientation","vertical"},
+        {"rect",{{"height",0},{"width",0},{"x",0},{"y",0}}},
+        {"sticky",false},
+        {"swallows",json::array()},
+        {"type","root"},
+        {"urgent",false},
+        {"window_icon_padding",-1},
+        {"window_rect",{{"height",0},{"width",0},{"x",0},{"y",0}}}
+    };
+
+    ASSERT_EQ(j, j2);
 }
